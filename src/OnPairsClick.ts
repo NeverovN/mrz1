7149d6a10196d4e72be08aec61487e74842d7ce4
NeverovN <neverovvnn@gmail.com>
2021-04-18 01:59:28 +0300

    removed unused functions

--- src/OnPairsClick.ts
+++
@@ -1,8 +0,0 @@
-export default function onPairClick(): number | void {
-    const input = document.getElementById("pairCountInput") as HTMLInputElement;
-    const inputValue: string = input.value;
-
-    if (!Number.isNaN(Number.parseInt(inputValue))) {
-        return  Number.parseInt(inputValue);
-    }
-}
--- src/RenderPairsInput.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function renderPairsInput(args: any) {
-    console.log(args);
-    if (typeof args[0] === "undefined"){
-        return (<div></div>);
-    }
-    return args;
-}

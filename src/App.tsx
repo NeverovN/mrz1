0a563c714d8ab921c6e3839e50adbfc7871d972a
NeverovN <neverovvnn@gmail.com>
2021-04-22 13:57:34 +0300

    reworking logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
-import React, { useState } from 'react';
+import React, { ChangeEventHandler, useState, useRef } from 'react';
 import './App.css';
 import { addPairs, getInputValue } from './AddPairs'
 import countConveyor from './CountConveyor'
+import { ChangeEvent } from 'react';
+
+interface IProps_Square {
+  message: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
 
 function App() {
-  const [renderPairForms, setRenderPairForms]: any = useState();
+  const [pairCountInput, setPairCountInput]= useState<string>();
+  const [renderPairForms, setRenderPairForms] = useState<React.ReactNode>();
   const [renderConveyorInput, setRenderConveyorInput]: any = useState();
-  const [pairCount, setPairCount]: any = useState(0);
+  const [pairs, setPairs] = useState<object>();
+  const pairCount = useRef<number>();
+
+  function inputChangeHandler(e: React.FormEvent<HTMLInputElement>) {
+    if (!Number.isNaN(Number.parseInt(e.currentTarget.value)) || e.currentTarget.value === "" ) {
+      setPairCountInput(e.currentTarget.value);
+    } else {
+      alert("VALUE MUST BE INTEGER NUMBER");
+    }
+  }
+
+  function pairElementChangeHandler(e: React.FormEvent<HTMLInputElement>) { //does not work
+    const id = e.currentTarget.id;
+    const num = id.substring(0, id.length - 1);
+    const index = id.substring(id.length - 1, id.length);
+    setPairs({
+      ...pairs,
+      num: {
+        index: e.currentTarget.value,
+      },
+    });
+    console.log(pairs);
+  }
+
+  function createPairsGrid(count: number) {
+    let pairs = new Array<React.ReactNode>(count).fill(undefined);
+    pairs = pairs.map((el, index) => el = [<input type="text" id={`${index}1`} onChange={ pairElementChangeHandler }/>, <input type="text" id={`${index}2`}/>])
+    return pairs;
+  }
+
+  function pairCountSubmitHandler(e: React.MouseEvent<HTMLButtonElement>) {
+    if (typeof pairCountInput !== "undefined" && pairCountInput !== "") {
+      pairCount.current = Number.parseInt(pairCountInput);
+    } else {
+      alert("SET PAIR COUNT");
+      return;
+    }
+    
+    setRenderPairForms(createPairsGrid(pairCount.current));
+
+  }
 
   return (
     <div className="App">
@@ -15,31 +62,28 @@ function App() {
           Лаба 1
         </p>
         <div className="App-input">
-          {/* <text>введите количество пар</text> */}
+          <text>введите количество пар</text>
           <br />
-          <form onSubmit={(event) => {
-            event.preventDefault();
-            const count = getInputValue();
-            if (typeof addPairs() !== "undefined" && typeof count !== "undefined") {
-              setPairCount(Number.parseInt(count));
-              setRenderPairForms(addPairs());
-            }
-          }}>
+          <form>
             <input id="pairCountInput"
               type="text"
               size={10}
+              value={pairCountInput}
+              onChange={inputChangeHandler}
             />
             <br />
-            <input type="submit" className="Button" />
+            <button
+              type="button"
+              className="Button"
+              onClick={pairCountSubmitHandler}
+            >
+              TEXT
+            </button>
           </form>
           <br />
         </div>
         <form
           className="Pairs-input"
-          onSubmit={(event) => {
-            event.preventDefault();
-            setRenderConveyorInput(countConveyor(pairCount));
-          }}
         >
           {renderPairForms}
         </form>
